fd29eb947573b780b82366e92ba5ca2570430d76
fabiomirandaa <fabiomirandadev@gmail.com>
2021-05-26 16:29:21 -0300

    2 files modified, 3 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
 import { InMemoryDataService } from './in-memory-data.service';
 
 import { AppComponent } from './app.component';
+import { TitleComponent } from './title/title.component';
 
 @NgModule({
   imports: [
@@ -16,7 +17,7 @@ import { AppComponent } from './app.component';
       dataEncapsulation: false
     })
   ],
-  declarations: [AppComponent],
+  declarations: [AppComponent, TitleComponent],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
